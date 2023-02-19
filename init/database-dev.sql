16af1b33c9b82065ded0c61ddd6c8315e7206b6d
Cesxhin <mattia.ceschin@gmail.com>
2023-02-19 19:09:55 +0100

    remove tables of the descriptions (manga and anime) for postgres and now it is used schemas with mongo and postgres (for only in dev)

---
+++ init/database-dev.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE animemanga OWNER root;
+\connect animemanga;
+
+/*video*/
+CREATE TABLE episode
+(
+    id varchar(500) primary key not null,
+    VideoId  varchar(250) not null,
+    numberEpisodeCurrent int not null,
+    numberSeasonCurrent int default 1,
+    stateDownload varchar(100),
+    percentualDownload int default 0,
+
+    urlVideo text default null,
+
+    baseUrl varchar(250) default null,
+    playlist varchar(250),
+    resolution varchar(250),
+    playlistSources varchar(250),
+    startNumberFrame int,
+    endNumberFrame int
+);
+
+CREATE TABLE episodeRegister
+(
+    episodeId varchar(500) primary key REFERENCES episode(id) ON DELETE CASCADE not null,
+    episodePath varchar(500),
+    episodeHash varchar(64)
+);
+
+/*book*/
+CREATE TABLE chapter
+(
+    id varchar(500) primary key not null,
+    nameManga varchar(250) not null,
+    currentVolume decimal not null,
+    currentChapter decimal not null,
+    numberMaxImage integer not null,
+    urlPage text not null,
+    stateDownload varchar(100),
+    percentualDownload int default 0,
+
+    nameCfg text not null
+);
+
+CREATE TABLE chapterRegister
+(
+    chapterId varchar(500) primary key REFERENCES chapter(id) ON DELETE CASCADE not null,
+    chapterPath varchar[],
+    chapterHash varchar[]
+);
